00d036160b8bf646de5f3210fb7a789c1cdfcc7d
Aun Mirza <71368056+aunmirza@users.noreply.github.com>
2024-10-08 17:44:48 +0200

    Initial Commit

---
+++ Card/CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardApp.swift
+//  Card
+//
+//  Created by Muhammad Aun e Ali Mirza on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
